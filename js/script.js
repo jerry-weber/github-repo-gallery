34c1a482ed8c07c6f3176f1701603706b7e9bd6f
jerry_w <jweber@gmx.com>
2023-08-02 17:39:59 -0500

    Added search box option to top of gallery

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 // Selecting DOM elements
 const userOverview = document.querySelector(".overview");
-const username = "jerry-weber";
+const githubUsername = "jerry-weber";
 const repositoryList = document.querySelector(".repo-list");
 const allRepositoriesContainer = document.querySelector(".repos");
-const repositoryData = document.querySelector(".repo-data");
+const repositoryDetails = document.querySelector(".repo-data");
+const viewRepositoriesButton = document.querySelector(".view-repos");
+const searchInput = document.querySelector(".filter-repos");
 
 // Function to fetch user information from GitHub API
 const fetchUserInfo = async function () {
   const userInfoResponse = await fetch(
-    `https://api.github.com/users/${username}`
+    `https://api.github.com/users/${githubUsername}`
   );
   const userData = await userInfoResponse.json();
   displayUserInfo(userData);
@@ -33,11 +35,11 @@ const displayUserInfo = function (userData) {
     </div>
   `;
   userOverview.append(userInfoDiv);
-  fetchUserRepos();
+  fetchUserRepositories(githubUsername);
 };
 
 // Function to fetch user repositories from GitHub API
-const fetchUserRepos = async function () {
+const fetchUserRepositories = async function (username) {
   const userReposResponse = await fetch(
     `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
   );
@@ -47,6 +49,7 @@ const fetchUserRepos = async function () {
 
 // Function to display user repositories on the page
 const displayRepositories = function (repositories) {
+  searchInput.classList.remove("hide");
   for (const repository of repositories) {
     const repoItem = document.createElement("li");
     repoItem.classList.add("repo");
@@ -59,29 +62,30 @@ const displayRepositories = function (repositories) {
 repositoryList.addEventListener("click", function (event) {
   if (event.target.matches("h3")) {
     const repositoryName = event.target.innerText;
-    fetchRepoInfo(repositoryName);
+    fetchRepositoryInfo(repositoryName);
   }
 });
 
 // Function to fetch repository information from GitHub API
-const fetchRepoInfo = async function (repositoryName) {
+const fetchRepositoryInfo = async function (repositoryName) {
   const repoInfoResponse = await fetch(
-    `https://api.github.com/repos/${username}/${repositoryName}`
+    `https://api.github.com/repos/${githubUsername}/${repositoryName}`
   );
   const repositoryInfo = await repoInfoResponse.json();
   // Fetch languages used in the repository
   const languagesResponse = await fetch(repositoryInfo.languages_url);
-  const languagesData = await languagesResponse.json();
+  const languageData = await languagesResponse.json();
   // Convert languages data to an array
-  const languages = Object.keys(languagesData);
+  const languages = Object.keys(languageData);
 
-  displayRepoInfo(repositoryInfo, languages);
+  displayRepositoryInfo(repositoryInfo, languages);
 };
 
 // Function to display repository information on the page
-const displayRepoInfo = function (repositoryInfo, languages) {
-  repositoryData.innerHTML = "";
-  repositoryData.classList.remove("hide");
+const displayRepositoryInfo = function (repositoryInfo, languages) {
+  viewRepositoriesButton.classList.remove("hide");
+  repositoryDetails.innerHTML = "";
+  repositoryDetails.classList.remove("hide");
   allRepositoriesContainer.classList.add("hide");
   const repoInfoDiv = document.createElement("div");
   repoInfoDiv.innerHTML = `
@@ -93,5 +97,28 @@ const displayRepoInfo = function (repositoryInfo, languages) {
       repositoryInfo.html_url
     }" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
   `;
-  repositoryData.append(repoInfoDiv);
+  repositoryDetails.append(repoInfoDiv);
 };
+
+// Event listener for the view repositories button
+viewRepositoriesButton.addEventListener("click", function () {
+  allRepositoriesContainer.classList.remove("hide");
+  repositoryDetails.classList.add("hide");
+  viewRepositoriesButton.classList.add("hide");
+});
+
+// Event listener for dynamic search
+searchInput.addEventListener("input", function (event) {
+  const searchText = event.target.value;
+  const repos = document.querySelectorAll(".repo");
+  const searchLowerText = searchText.toLowerCase();
+
+  for (const repo of repos) {
+    const repoLowerText = repo.innerText.toLowerCase();
+    if (repoLowerText.includes(searchLowerText)) {
+      repo.classList.remove("hide");
+    } else {
+      repo.classList.add("hide");
+    }
+  }
+});
